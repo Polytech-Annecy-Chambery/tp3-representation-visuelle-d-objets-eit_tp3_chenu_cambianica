1ddde519bafc861b09e8841e945d774010b1da01
NicoCHENU <95041442+NicoCHENU@users.noreply.github.com>
2022-01-17 17:56:35 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,7 @@ Created on Thu Nov 16 19:47:50 2017
 @author: lfoul
 """
 
-from Configuration import Configuration
+from Configuration  import Configuration 
 from Section import Section
 from Wall import Wall
 from Door import Door
@@ -16,12 +16,12 @@ import copy
 
 
 def Q1a():
-    pass
+    return Configuration()
     
 def Q1b_f():
     return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}). \
         setParameter('xAxisColor', [1, 1, 0]). \
-        setParameter('yAxisCo lor', [0,1,1]). \
+        setParameter('yAxisColor', [0,1,1]). \
         display()
         
 def Q2b():
@@ -37,30 +37,30 @@ def Q2c():
             )
 
 def Q3a():
-    pass  
+    return Configuration().add(
+            Wall({'position':[1,1,0],'width': 7,'height': 2.6,'edges': True, 'orientation': 90})  )
 
 def Q4a():
-    # Ecriture en utilisant des variables : A compléter
-    wall1 = Wall(...)
-    wall2 = Wall(...)
-    wall3 = Wall(...)
-    wall4 = Wall(...)  
+    wall1 = Wall({'position':[0,0,0],'width': 7,'height': 2.6,'edges': True, 'orientation': 0})
+    wall2 = Wall({'position':[-6.8,6.8,0],'width': 7,'height': 2.6,'edges': True, 'orientation': 180})
+    wall3 = Wall({'position':[-6.8,0,0],'width': 7,'height': 2.6,'edges': True, 'orientation': 90})
+    wall4 = Wall({'position':[0,6.8,0],'width': 7,'height': 2.6,'edges': True, 'orientation': -90})  
     house = House({'position': [-3, 1, 0], 'orientation':0})
     house.add(wall1).add(wall3).add(wall4).add(wall2)
     return Configuration().add(house)   
     
 def Q5a():  
     # Ecriture avec mélange de variable et de chaînage    
-    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
-    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})    
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thick':0.2, 'color': [0.7, 0.7, 0.7]})
+    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thick':0.2, 'color': [0.7, 0.7, 0.7]})    
     return Configuration().add(opening1).add(opening2)
     
 def Q5b():  
     # Ecriture avec mélange de variable et de chaînage   
     section = Section({'width':7, 'height':2.6})
-    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
-    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
-    opening3 = Opening({'position': [4, 0, 1.7], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thick':0.2, 'color': [0.7, 0.7, 0.7]})
+    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thick':0.2, 'color': [0.7, 0.7, 0.7]}) 
+    opening3 = Opening({'position': [4, 0, 1.7], 'width':1.25, 'height':1, 'thick':0.2, 'color': [0.7, 0.7, 0.7]}) 
     
     print(section.canCreateOpening(opening1))
     print(section.canCreateOpening(opening2))    
@@ -94,13 +94,13 @@ def Q6():
 def main():
     # Enlever un des commentaires pour la question traitée
     
-    configuration = Q1a()
+    #configuration = Q1a()
     # configuration = Q1b_f()
-    # configuration = Q2b()
-    # configuration = Q2c()
-    # configuration = Q3a()
-    # configuration = Q4a()
-    # configuration = Q5a()
+    #configuration = Q2b()
+    #configuration = Q2c()
+    #configuration = Q3a()
+    #configuration = Q4a()
+    configuration = Q5a()
     # configuration = Q5b()
     # configuration = Q5c1()
     # configuration = Q5c2() 
